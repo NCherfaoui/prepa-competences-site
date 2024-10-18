f19900f76824c09a6e5bc6060ce9b64b801ecee5
Nassim Cherfaoui <nassim.cherfaoui@gmail.com>
2024-10-18 20:04:18 +0200

    Refactorisation du fichier .gitignore et ajout de fichiers exclus

--- tests/Main.test.jsx
+++ tests/Main.test.jsx
@@ -1,9 +1,8 @@
 // tests/Main.test.jsx
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import Home from '../src/pages/Home';
-
-test('renders home page', () => {
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Home from "../src/pages/Home";
+test("renders home page", () => {
   render(
     <MemoryRouter>
       <Home />
--- tests/ThemeToggle.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import ThemeToggle from '../src/components/ThemeToggle';
-
-test('renders theme toggle button', () => {
-  render(<ThemeToggle />);
-  const toggleButton = screen.getByRole('button');
-  expect(toggleButton).toBeInTheDocument();
-});
